93f5c15366371947b2e8e5bc9e0b6595da3f5559
Pim van Stam <pim@svsnet.nl>
2018-01-31 18:30:22 +0100

    added set_available functions - not ready !

--- src/classes/Result.php
+++ src/classes/Result.php
@@ -48,6 +48,46 @@ class Result
             return True;            
     }
     
+    function clear_results() {
+        $sql = 'UPDATE workshop
+                    SET votes = (SELECT SUM(participant)
+                                 FROM workshop_participant
+                                 WHERE workshop.id=workshop_participant.workshop_id),
+                        round_id = NULL,
+                        location_id = NULL,
+                        available = NULL';
+        $query=$this->db->prepare($sql);
+        $param = array ($login, $password);
+        try {
+            $query->execute($param);
+        } catch (PDOException $e){
+            echo $e->getMessage();
+            return False;
+        }
+        return True;
+    }
+    
+    /*
+     * set available field
+     */
+    function set_availables() {
+        $sql = 'SELECT id
+                FROM workshop
+                WHERE published=1
+                ORDER BY votes desc
+                LIMIT 0, $rounds;';
+        $query=$this->db->prepare($sql);
+        $param = array ($login, $password);
+        try {
+            $query->execute($param);
+        } catch (PDOException $e){
+            echo $e->getMessage();
+            return False;
+        }
+        return True;
+    }
+    
+    
     
     public function calculate_results($request, $response, $args) {
         
